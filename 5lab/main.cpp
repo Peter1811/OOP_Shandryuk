a0b7c0fc21871a54d009a257104dcb80674e1966
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2022-01-09 20:28:06 +0300

    Update main.cpp

--- 5lab/main.cpp
+++ 5lab/main.cpp
@@ -1,15 +1,15 @@
-#include "rectangle.h"
+#include "triangle.h"
 #include "tqueue.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  vector<Point> v{Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0)};
-  { TQueue_item<Rectangle> item(make_shared<Rectangle>(v)); }
-  TQueue<Rectangle> queue;
-  queue.push(make_shared<Rectangle>(v));
-  queue.push(make_shared<Rectangle>());
+  vector<Point> v{Point(0, 0), Point(0, 1), Point(1, 1)};
+  { TQueue_item<Triangle> item(make_shared<Triangle>(v)); }
+  TQueue<Triangle> queue;
+  queue.push(make_shared<Triangle>(v));
+  queue.push(make_shared<Triangle>());
   for (auto x : queue) {
     cout << *x << endl;
   }
